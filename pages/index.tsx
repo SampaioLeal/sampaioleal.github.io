7b7c5fe5803d8acd28def36234f1e1a40c2b04ec
Sampaio Leal <samp14hd@gmail.com>
2021-04-11 20:03:28 -0300

    Google verification meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,6 +39,10 @@ export default function Home({ helloMessage }) {
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="google-site-verification"
+          content="JCGi0d0RZVpYH_VSR39xTsHUA2xaOS2rxo3Lh5excww"
+        />
       </Head>
 
       <Container>
