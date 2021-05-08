aa69d2743f5048268b184a85dc790260242e7d39
Sampaio Leal <samp14hd@gmail.com>
2021-05-08 19:20:54 -0300

    Image Meta tag added

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,36 +42,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Antonio Sampaio - Portfólio</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="google-site-verification"
-          content="JCGi0d0RZVpYH_VSR39xTsHUA2xaOS2rxo3Lh5excww"
-        />
-        <meta property="og:site_name" content="Antonio Sampaio - Portfólio" />
-        <meta property="og:title" content="Antonio Sampaio - Portfólio" />
-        <meta
-          property="og:description"
-          content="Descobrindo a tecnologia desde os 14, fui introduzido ao
-          desenvolvimento Web e, desde então, meu foco é dominar as
-          tecnologias Node.JS e React/React Native, utilizando TypeScript
-          para um melhor ambiente de desenvolvimento e trabalho em equipe."
-        />
-        <meta name="author" content="Antonio Sampaio Leal Neto" />
-        <meta property="og:locale" content="pt_BR" />
-        <meta http-equiv="content-language" content="pt-br" />
-        <meta
-          name="keywords"
-          content="antonio, sampaio, leal, desenvolvedor, fullstack, desenvolvimento, sampaioleal"
-        />
-      </Head>
-
       <Container>
         <MainContent>
           <MainGreetings>
